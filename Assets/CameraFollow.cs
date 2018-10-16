e44ab4559e747823ab270158b7356dcf07640461
Unknown <kalebismadman@gmail.com>
2018-10-16 18:29:43 +1100

    Project changed

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform player;
+    public Transform defaultLooking;
+
+    public Transform ballObject;
+
+    public float rangeSet;
+    public float lookRange;
+    public float damping;
+    public float smoothSpeed = 0.125f;
+
+    bool ballNear = false;
+    public Vector3 offset;
+
+
+    void FixedUpdate()
+    {
+        lookRange = Vector3.Distance(ballObject.position, transform.position);
+
+        LookAtMe();
+
+        Vector3 desiredPosition = player.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        LookAtObject();
+    }
+
+    void LookAtObject()
+    {
+        if (ballNear == false)
+        {
+            Quaternion rotation = Quaternion.LookRotation(defaultLooking.position - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
+        } else
+        {
+            Quaternion rotation = Quaternion.LookRotation(ballObject.position - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
+        }
+    }
+
+    void LookAtMe()
+    {
+        if (lookRange < rangeSet)
+        {
+            ballNear = true;
+        }
+
+        if (lookRange > rangeSet)
+        {
+            ballNear = false;
+        }
+    }
+
+}
---
+++ Assets/Drive.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Drive : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 2.0f;
+    [SerializeField]
+    private float rotationSpeed = 30.0f;
+    [SerializeField]
+    private float jumpSpeed = 5.0f;
+
+    bool isgrounded = true;
+
+    void Update()
+    {
+        float translation = CrossPlatformInputManager.GetAxisRaw("Vertical") * speed;
+        float rotation = CrossPlatformInputManager.GetAxisRaw("Horizontal") * rotationSpeed;
+        float jumping = Input.GetAxisRaw("Jump") * jumpSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        jumping *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+        transform.Translate(0, jumping, 0);
+
+    }
+}
